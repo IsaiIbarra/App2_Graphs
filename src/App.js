cb4db6db39ea6a7d4a6f8a16606cf924e532fe94
IsaiIbarra <isaiibarra1010@gmail.com>
2022-06-04 22:03:51 -0500

    Base de la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  CContainer,
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CCardText,
+  CButton,
+} from '@coreui/react';
 
 function App() {
+  useEffect(() => {}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CContainer>
+      <CCard className='m-5'>
+        <CCardHeader>Restaurants</CCardHeader>
+        <CCardBody></CCardBody>
+      </CCard>
+    </CContainer>
   );
 }
 
